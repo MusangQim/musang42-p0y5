752b64e77b6ea7cb1d1391ba22e74e26e1fb03cf
MusangQim <musangqim03@gmail.com>
2026-05-02 16:03:15 +0800

    update: update methods at main class

--- ex0/data_processor.py
+++ ex0/data_processor.py
@@ -1,7 +1,51 @@
 #!/usr/bin/env python3
 from abc import ABC, abstractmethod
+from typing import Any
+
+
+class DataProcessor(ABC):
+    # check kalau data appropriate tak for current data processor
+    @abstractmethod
+    def validate(self, data: Any) -> bool:
+        pass
+    # process the input data
+    @abstractmethod
+    def ingest(self, data: Any) -> None:
+        pass
+    # output ingested data
+    @abstractmethod
+    def output(self) -> tuple[int, str]:
+        pass
+
+
+# ingest int, float and lists of both types(include mix-type list)
+class NumericProcessor(DataProcessor):
+    def ingest(self, data: Any) -> None:
+        return super().ingest(data)
+    
+    def output(self) -> tuple[int, str]:
+        return super().output()
+
+
+class TextProcessor(DataProcessor):
+    def ingest(self, data: Any) -> None:
+        return super().ingest(data)
+    
+    def output(self) -> tuple[int, str]:
+        return super().output()
+
+
+class LogProcessor(DataProcessor):
+    def ingest(self, data: Any) -> None:
+        return super().ingest(data)
+    
+    def output(self) -> tuple[int, str]:
+        return super().output()
+
 
 def main() -> None:
+    print("=== Code Nexus - Data Processor ===\n")
+
 
 
 if __name__ == "__main__":
