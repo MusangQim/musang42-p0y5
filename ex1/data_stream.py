873503da741d1b404c716abb5c5c446153e9acf6
MusangQim <musangqim03@gmail.com>
2026-05-08 12:41:08 +0800

    update: Skeleton DataStream + Main function fix errors + flake8 + mypy DONE

--- ex1/data_stream.py
+++ ex1/data_stream.py
@@ -2,6 +2,7 @@
 from abc import ABC, abstractmethod
 from typing import Any
 
+
 class DataProcessor(ABC):
     def __init__(self) -> None:
         self._storage: list[tuple[int, str]] = []
@@ -105,23 +106,36 @@ class LogProcessor(DataProcessor):
                     self._rank += 1
         else:
             raise TypeError("Improper log data")
-        
 
-class DataStream(ABC):
+
+class DataStream:
     def __init__(self) -> None:
-        self._processors = []
-    
+        self._processors: list[DataProcessor] = []
 
     def register_processor(self, proc: DataProcessor) -> None:
-        self._proc = proc.append(DataProcessor)
+        self._processors.append(proc)
 
-
-    def process_stream(self, stream: list[typing.Any]) -> None:
+    def process_stream(self, stream: list[Any]) -> None:
         for element in stream:
             routed = False
-            for processor in _processor
+            for processor in self._processors:
+                if processor.validate(element) is True:
+                    processor.ingest(element)
+                    routed = True
+                    break
+            if not routed:
+                print(f"DataStream error - Can't process"
+                      f" element in stream: {element}")
 
     def print_processors_stats(self) -> None:
+        if not self._processors:
+            print("No processor found, no data")
+        for processor in self._processors:
+            name = type(processor).__name__
+            total = processor._rank
+            remaining = len(processor._storage)
+            print(f"{name}: total {total} items processed,"
+                  f" remaining {remaining} on processor")
 
 
 def main() -> None:
@@ -129,19 +143,46 @@ def main() -> None:
     stream = DataStream()
     print("Initialize Data Stream...")
     print("== DataStream statisctics ==")
-    stream.print_processor_stats()
-
-    print("Registering Numeric Processor\n")
+    stream.print_processors_stats()
+# === NumericProcessor running.... ===
+    print("\nRegistering Numeric Processor\n")
     stream.register_processor(NumericProcessor())
-    n1_batch = ['Hello world', [3.14, -1, 2.71],
-                [{'log_level': 'WARNING', 'log_message': 'Telnet access! Use ssh instead'},
-                 {'log_level': 'INFO', 'log_message': 'User wil is connected'}],
-                 42, ['Hi', 'five']]
+    log_batch = [{'log_level': 'WARNING',
+                  'log_message': 'Telnet access! Use ssh instead'},
+                 {'log_level': 'INFO',
+                  'log_message': 'User wil is connected'}
+                 ]
+    n1_batch = [
+        'Hello world',
+        [3.14, -1, 2.71],
+        log_batch,
+        42,
+        ['Hi', 'five']
+    ]
     print(f"Send first batch of data on stream: {n1_batch}")
     stream.process_stream(n1_batch)
     print("== DataStream statistics ==")
     stream.print_processors_stats()
+# === Other data Processor running.... ===
+    print("\nRegistering other data processors")
+    stream.register_processor(TextProcessor())
+    stream.register_processor(LogProcessor())
+    print("Send the same batch again")
+    stream.process_stream(n1_batch)
+    print("== DataStream statistics ==")
+    stream.print_processors_stats()
+# === Consuming elements.... ===
+    print("\nConsume some elements from the "
+          "data processors: Numeric 3, Text 2, Log 1")
+    for i in range(3):
+        stream._processors[0].output()
+    for j in range(2):
+        stream._processors[1].output()
+    for k in range(1):
+        stream._processors[2].output()
+    print("== DataStream statistics ==")
+    stream.print_processors_stats()
 
-    
+
 if __name__ == "__main__":
     main()
