0b86858ff094f4bd0621efae96642ac34506d192
MusangQim <musangqim03@gmail.com>
2026-05-08 11:36:59 +0800

    update: add previous exercise

--- ex1/data_stream.py
+++ ex1/data_stream.py
@@ -2,6 +2,110 @@
 from abc import ABC, abstractmethod
 from typing import Any
 
+class DataProcessor(ABC):
+    def __init__(self) -> None:
+        self._storage: list[tuple[int, str]] = []
+        self._rank = 0
+
+    # check kalau data appropriate tak for current data processor
+    @abstractmethod
+    def validate(self, data: Any) -> bool:
+        pass
+
+    # process the input data
+    @abstractmethod
+    def ingest(self, data: Any) -> None:
+        pass
+
+    # output ingested data
+    def output(self) -> tuple[int, str]:
+        item_saved: tuple[int, str] = self._storage.pop(0)
+        return item_saved
+
+
+# ingest int, float and lists of both types(include mix-type list)
+class NumericProcessor(DataProcessor):
+    def validate(self, data: Any) -> bool:
+        if isinstance(data, int):
+            return True
+        elif isinstance(data, float):
+            return True
+        elif isinstance(data, list):
+            for datalist in data:
+                check_int = isinstance(datalist, int)
+                check_float = isinstance(datalist, float)
+                if not check_int and not check_float:
+                    return False
+            return True
+        else:
+            return False
+
+    def ingest(self, data: Any) -> None:
+        if self.validate(data):
+            if isinstance(data, list):
+                for item in data:
+                    self._storage.append((self._rank, str(item)))
+                    self._rank += 1
+                else:
+                    self._storage.append((self._rank, str(data)))
+                    self._rank += 1
+        else:
+            raise TypeError("Improper numeric data")
+
+
+# ingest str and lists of str
+class TextProcessor(DataProcessor):
+    def validate(self, data: Any) -> bool:
+        if isinstance(data, str):
+            return True
+        elif isinstance(data, list):
+            for textlist in data:
+                if not isinstance(textlist, str):
+                    return False
+            return True
+        else:
+            return False
+
+    def ingest(self, data: Any) -> None:
+        if self.validate(data):
+            if isinstance(data, list):
+                for item in data:
+                    self._storage.append((self._rank, str(item)))
+                    self._rank += 1
+                else:
+                    self._storage.append((self._rank, str(data)))
+                    self._rank += 1
+        else:
+            raise TypeError("Improper text data")
+
+
+# ingest dict and lists of dict
+class LogProcessor(DataProcessor):
+    def validate(self, data: Any) -> bool:
+        if isinstance(data, dict):
+            return True
+        elif isinstance(data, list):
+            for loglist in data:
+                if not isinstance(loglist, dict):
+                    return False
+            return True
+        else:
+            return False
+
+    def ingest(self, data: Any) -> None:
+        if self.validate(data):
+            if isinstance(data, dict):
+                format_dict = f"{data['log_level']}: {data['log_message']}"
+                self._storage.append((self._rank, format_dict))
+                self._rank += 1
+            elif isinstance(data, list):
+                for item in data:
+                    format_list = f"{item['log_level']}: {item['log_message']}"
+                    self._storage.append((self._rank, format_list))
+                    self._rank += 1
+        else:
+            raise TypeError("Improper log data")
+        
 
 class DataStream(ABC):
     def __init__(self) -> None:
